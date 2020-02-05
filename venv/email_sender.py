8a1b7c4670e7d2a59e4f261943cf0cf8ed5182c7
IamSeo-Dev <seoyooseo0@gmail.com>
2020-02-04 21:26:45 -0800

    ver 0.1 Test Done

--- venv/email_sender.py
+++ venv/email_sender.py
@@ -2,7 +2,7 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from datetime import date
-import config
+#import config
 
 
 def send_email(body):
