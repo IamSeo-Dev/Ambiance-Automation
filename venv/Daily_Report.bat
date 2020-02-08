abc83a9a7fb78ccb800ff49f3b034670b38dd03f
IamSeo-Dev <seoyooseok0@gmail.com>
2020-02-07 16:46:28 -0800

    UI Improvement Test

--- venv/Daily_Report.bat
+++ venv/Daily_Report.bat
@@ -1,1 +1,1 @@
-"c:\users\yooseokseo.AMBIANCE\Appdata\Local\Programs\Python\Python38-32\python.exe" "c:\users\yooseokseo.AMBIANCE\Documents\My Work\[My Project]\Daily_Report_Automation\venv\app.py"
+"c:\users\yooseokseo.AMBIANCE\Appdata\Local\Programs\Python\Python38-32\python.exe" "c:\users\yooseokseo.AMBIANCE\Documents\My Work\[My Project]\Ambiance-Automation\venv\app.py"
